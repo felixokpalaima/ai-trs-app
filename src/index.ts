0a86badabbd09e4e31754b073ed16b43ab51a299
felixokpalaima <felixokpalaima@gmail.com>
2024-05-21 20:26:22 +0200

    add loggerOD

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import path from 'path';
 import express, { Request, Response } from 'express';
+import { createLogger, format, transports } from 'winston';
+
 import recommendationRouter from './routes';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.Console({
+      format: format.simple()
+    })
+  ]
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,8 +26,13 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request on ${req.url}`);
+  next();
+});
 
 app.get('/', (_req, res) => {
+  logger.debug('Handling GET request for /');
   res.render('index', { recommendations: [] });
 });
 
@@ -24,4 +44,5 @@ app.all('*', (_req: Request, res: Response) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Server is running on http://localhost:${PORT}`);
 });
